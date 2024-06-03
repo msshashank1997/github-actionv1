64e0bc66f98fd9157329b6a6b976b504722dcbe6
shashank <139876587+msshashank1997@users.noreply.github.com>
2024-06-03 22:57:37 +0530

    Update deploy-webapp.ps1

--- deploy-webapp.ps1
+++ deploy-webapp.ps1
@@ -2,13 +2,31 @@
 param(
     [Parameter(Mandatory = $True)]
     [string]
-    $deploymentid
+    $deploymentid,
+    [string]
+    $Username,
+    [string]
+    $Password
 )
 
 $continername = "testcontinerinstanse" + $deploymentid
 $webappname = "webpage" + $deploymentid
 $rgname = "testcontinerinstanse"
 
+az login -u $Username -p $Password
+Write-Output "Done"
+Write-Output ""
+#endregion
+
+#region Subscription
+#This sets the subscription the resources will be created in
+
+Write-Output "Setting default azure subscription..."
+az account set `
+    --subscription "$azureSubscriptionName"
+Write-Output "Done"
+Write-Output ""
+#endregion
 
 az acr login --name 
 Write-Output "Done"
